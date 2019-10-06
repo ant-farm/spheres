310f6cf4aaad3a998232030e915191c787c4322d
Anthony Torres <anthony_torres@me.com>
2019-10-06 17:22:59 -0500

    added velocity to the x axis

--- js/app.js
+++ js/app.js
@@ -20,19 +20,21 @@ class Sphere{
 //Since the animate function will continuelly generate an image in the same location due to the hard coded x and y values, creating a variable so that number can change is what i did here
 let x = 200;
 let y = 200;
+let velocityX = 10;
+
 //this function will be used to generate movement
 function animate(){
 
 	requestAnimationFrame(animate);
 	//this will clear the canvas each time the function runs. 700 numbers are the total width and height of the canvas itself
-	ctx.clearRect(0,0, 700, 700)
+	ctx.clearRect(0,0, 700, 700);
 	//created a circle
 	ctx.beginPath();
 	ctx.arc(x, y, 5, 0, Math.PI * 2);
 	ctx.fillStyle = '#ff0000';
 	ctx.fill();
 	// adding one to the x value every time the function runs. This creates movement 
-	x += 1
+	x += velocityX
 	
 }
 const game ={
