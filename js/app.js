55f668794adf64995f0611a7f8d41416b575b58b
Anthony Torres <anthony_torres@me.com>
2019-10-06 17:06:58 -0500

    created a circle with animate

--- js/app.js
+++ js/app.js
@@ -4,34 +4,49 @@ const canvas = document.getElementById('my-canvas');
 const ctx = canvas.getContext('2d');
 
 
+// Circle circle dot dot
 
 //class
+class Sphere{
+	constructor(){
 
+	}
+
+}
 
 
 // <-------------------------------------------->
-ctx.beginPath();
-ctx.arc(100,100, 5, 0, Math.PI * 2);
-ctx.fillStyle = '#ff0000';
-ctx.stroke();
 
+
+//this function will be used to generate movement
+function animate(){
+
+	requestAnimationFrame(animate);
+
+	//created a circle
+	ctx.beginPath();
+	ctx.arc(100,100, 5, 0, Math.PI * 2);
+	ctx.fillStyle = '#ff0000';
+	ctx.fill();
+}
 const game ={
 
 
 }
 
+animate()
 // ----------------------------------------
 // EVENT LISTENERS
 
-document.getElementById('start-game').addEventListener('click', (event) => {
-	game.generateSpheres()
-})
-document.getElementById('movement').addEventListener('click', (event) => {
-	animate()
-})
-document.getElementById('start-over').addEventListener('click', (event) => {
-	game.clearCanvas();
-})
+// document.getElementById('start-game').addEventListener('click', (event) => {
+// 	game.generateSpheres()
+// })
+// document.getElementById('movement').addEventListener('click', (event) => {
+// 	animate()
+// })
+// document.getElementById('start-over').addEventListener('click', (event) => {
+// 	game.clearCanvas();
+// })
 
 
 
