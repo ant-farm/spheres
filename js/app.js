de7e2db24a83160a7d435c2cd50283a538a96bf6
Anthony Torres <anthony_torres@me.com>
2019-10-06 16:59:59 -0500

    restarting code, created circle

--- js/app.js
+++ js/app.js
@@ -1,59 +1,37 @@
 
 //canvas set up
 const canvas = document.getElementById('my-canvas');
-console.log(canvas) // cool now we have the canvas
-
-// the "context" is what you actually draw on -- you basically always need it
 const ctx = canvas.getContext('2d');
-console.log(ctx); // cool, our rendering context is set up
+
 
 
 //class
-class Sphere {
-	constructor(){
-		this.x = Math.floor(Math.random() * 600),
-		this.y = Math.floor(Math.random() * 600),
-		this.radius = 5,
-		this.color = '#cc5500',
-		this.speed = 10
-	}
-	draw(){
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
-		ctx.fillStyle = this.color
-		ctx.fill()
-	}
-	move(){
 
-	}
-}
+
 
 // <-------------------------------------------->
+ctx.beginPath();
+ctx.arc(100,100, 5, 0, Math.PI * 2);
+ctx.fillStyle = '#ff0000';
+ctx.stroke();
 
 const game ={
-	//created a function that will generates spheres
-	generateSpheres(){
-		for(let i = 0; i < 50; i++){
-			let spheres = new Sphere
-			spheres.draw()
-		}
-	},
 
-	clearCanvas(){
-		ctx.clearRect(0, 0, canvas.width, canvas.height)
-	}
+
 }
-
-
 
 // ----------------------------------------
 // EVENT LISTENERS
 
 document.getElementById('start-game').addEventListener('click', (event) => {
-	game.generateSpheres();
+	game.generateSpheres()
+})
+document.getElementById('movement').addEventListener('click', (event) => {
+	animate()
 })
 document.getElementById('start-over').addEventListener('click', (event) => {
 	game.clearCanvas();
 })
+
 
 
